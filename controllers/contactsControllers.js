107e84611e4d99936a18a97aa10007d6677fb894
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-03-09 08:52:14 +0200

    1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,31 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const result = await contactsService.listContacts();
+  res.status(200).json(result);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.getContactById(id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.status(200).json(result);
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await contactsService.removeContact(id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.status(200).json(result);
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  const result = await contactsService.addContact(req.body);
+};
 
 export const updateContact = (req, res) => {};
---
+++ services/contactsServices.js
@@ -0,0 +1,52 @@
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("/db", "contacts.json");
+
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const contactWithId = contacts.find((item) => item.id === contactId);
+  return contactWithId || null;
+};
+
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(idx, 1);
+  await updateContacts(contacts);
+  return result;
+};
+
+export const addContact = async (data) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(4),
+    ...data,
+  };
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+export const updateContact = async (id, data) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === id);
+  if (idx === -1) {
+    return null;
+  }
+  contacts[idx] = { ...contacts[idx], ...data };
+  await updateContacts(contacts);
+  return contacts[idx];
+};
