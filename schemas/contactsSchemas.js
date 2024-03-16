728251118b94f9187ea40800373ae700b341943b
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-03-16 09:44:08 +0200

    1.1

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
+  email: Joi.string().email().required(),
   phone: Joi.string().required(),
 });
 
