4e34299f165547b2d18bf4ce4d3f77e39d805784
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-04-20 15:54:12 +0300

    rename function signupUser in authServices

--- services/authServices.js
+++ services/authServices.js
@@ -1,5 +1,5 @@
 import User from "../models/User.js";
 
-export const signup = (data) => User.create(data);
+export const signupUser = (data) => User.create(data);
 export const findUser = (findQuery) => User.findOne(findQuery);
 export const updateUser = (filter, data) => User.findOneAndUpdate(filter, data);
